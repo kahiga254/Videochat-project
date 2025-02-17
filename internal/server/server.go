1aa08e3c7650e8a4406dc8844a09e0001c27d510
kahiga254 <adamskahiga@gmail.com>
2025-02-17 06:02:21 +0300

    firts commit

---
+++ internal/server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"flag"
+	"os"
+	"time"
+	"videochat-project/internal/handlers"
+	w "videochat-project/pkg/webrtc"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/corsg"
+	"github.com/gofiber/fiber/v2/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+)
+
+var (
+	addr = flag.String("addr",":"+os.Getenv("PORT"), "")
+	cert = flag.String("cert","","")
+	key = flag.String("key","","") 
+)
+
+engine := html.New("./view",".html")
+app := fiber.New(fiber.Config{views: engine})
+app.use(logger.New())
+app.use(cors.New())
+
+func Run() error {
+	flag.Parse()
+
+	if *addr == ":"{
+		*addr =":3000"
+	}
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create",handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{HandshakeTimeout: 10*time.Second,}))
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket)) //semi colon
+	app.Get("/stream/:ssuid", handlers.stream)
+	app.Get("/stream/:ssuid/websocket", websocket.New(handlers.StreamWebsocket, websocket.Config{HandshakeTimeout: 10*time.Second,}))
+	app.Get("/stream/ssuid/chat/websocket", websocket.New(handlers.streamChatWebsocket)) //no colon
+	app.Get("/stream/:ssuid/viewer/websocket", websocket.New(handlers.StreamViewerWebsocket))
+	app.Static("/",".assets")
+
+	w.Rooms = make(map[string]*w.Room)
+	w.Streams = make(map[string]*w.Room)
+	go dispatchKeyFrames()
+	if *cert != ""{
+		return app.ListenTls(*addr, *cert, *key)
+	}
+		return app.Listen(*addr)
+}
+
+func dispatchKeyFrames(){
+	for range time.NewTicker(time.Second * 3).c{
+		for _, room := range w.Rooms{
+			room.Peers.DispatchKeyFrames()
+		}
+	}
+	
+}
