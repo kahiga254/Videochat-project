6f256fef5e220d3745104d7398d84f20ffc780ef
kahiga254 <adamskahiga@gmail.com>
2025-02-19 12:46:04 +0300

    second commit

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -1,13 +1,15 @@
 package handlers
 
-import(
+import (
 	"fmt"
-	w "videochat-project/pkg/webrtc"
 	"os"
 	"time"
+	w "videochat-project/pkg/webrtc"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	guuid "github.com/google/uuid"
+	"github.com/pion/webrtc/v3"
 )
 
 func RoomCreate(c *fiber.Ctx) error {
@@ -20,7 +22,19 @@ func Room(c *fiber.Ctx) error {
 		c.Status(400)
 		return nil
 	}
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION"{
+		ws = "wss"
+	}
 	uuid,suuid,_ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebSocketAddr":fmt.Sprintf("%s://%s/room%s/websocket",ws, c.Hostname(),uuid),
+		"RoomLink":fmt.Sprintf("%s://%s/room/%s", c.Protocol(), c.Hostname(),uuid),
+		"ChatWebSocketAddr":fmt.Sprintf("%s://%s/room/%s/chat/websocket",ws, c.Hostname(),uuid),
+		"viewerWebSocketAddr":fmt.Sprintf("%s://%s/room/%s/viewer/websocket",ws,c.Hostname()),
+		"StreamLink":fmt.Sprintf("%s://%s/stream/%s",c.Protocol(),c.Hostname(),suuid),
+		"Type":"room",
+	},"layouts/main")
 }
 
 func RoomWebsocket(c *websocket.Conn){
@@ -28,20 +42,61 @@ func RoomWebsocket(c *websocket.Conn){
 	if uuid == ""{
 		return
 	}
-
 	_,_, room := createOrGetRoom(uuid)
+	w.RoomConn(c, room.Peers)
 }
 
 func createOrGetRoom(uuid string)(string,string, *w.Room){
+	w.RoomsLock.Lock()
+	defer w.RoomsLock.Unlock()
+	h := sha256.new()
+	h.write([]byte(uuid))
+	suuid := fmt.Sprintf("%s", h.Sum(nil))
 
-}
+	if room := w.Rooms[uuid]; room != nil {
+		if _, ok := w.Streams[suuid]; !ok{
+			w.Streams[suuid] = room
+		}
+		return uuid, suuid, room
+	}
+	hub := chat.NewHub()
+	p := &w.Peers{}
+	p.TrackLocals = make(map[string]*webrtc.TrackLocalStaticRTP)
+	room := &w.Room{
+		Peers: p,
+		Hub: hub,
+	}
 
 func RoomViewerWebsocket(c *websocket.Conn){
+	uuid := c.Params("uuid")
+	if uuid == ""{
+		return
+	}
 
+	w.RoomsLock.Lock()
+	if peer, ok := w.Rooms[uuid]; ok{
+		w.RoomsLock.Unlock()
+		roomViewerConn(c, peer.Peers)
+		return
+	}
+	w.Rooms.Unlock()
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peers){
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
 
+	for {
+		select {
+		case <-ticker.c:
+			w,err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connection))))
+		}
+	}
 }
 
 type websocketMessage struct{
--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -9,6 +9,97 @@ type Peers struct{
 	TrackLocals map[string]*webrtc.TrackLocalStaticRTP
 }
 
+type peerConnectionState struct{
+	PeerConnection *webrtc.PeerConnection
+	websocket *ThreadSafeWriter
+}
+
+type ThreadSafeWriter struct{
+	coon *websocket.Conn
+	Mutex sync.Mutex
+}
+
+func (t *ThreadSafeWriter) WriteJSON(V interface()) error{
+	t.Mutex.Unlock()
+	return t.Conn.WriteJSON(v)
+}
+
+func (p *Peers) AddTrack(t *webrts.TrackRemote) *webrtc.TrackLocalStaticRTP{
+	p.ListLock.Lock()
+	p.SignalPeerConnections()
+}()
+
+trackLocal, errr := webrtc.NewTrackLocalStaticRTP(t.Codec().RTPCodecCapability, t.ID().StreamID())
+
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	p.TrackLocals[t.ID()] = trackLocal
+	return trackLocal
+
+func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP){
+	p.ListLock.Lock()
+	defer func(){
+		p.ListLock.Unlock()
+		p.SignalPeerConnections()
+	}()
+	delete(p.TrackLocals, t.ID())
+}
+
+func  (p *Peers)SignalPeerConnections(){
+	p.ListLock.Lock()
+	defer func(){
+		p.ListLock.Unlock()
+		p.dispatchKeyFrames()
+	}()
+
+	attemptSync := func() (tryAgain bool){
+		for i := range p.Connections{
+			if p.Connections[i].PeerConnection.ConnectionState() == webrtc.PeerConnectionStateClosed{
+				p.Connections = append(p.Connections[:i],np.Connections[i+1:]...)
+				logPrintln("a".p.Connections)
+				return true
+			}
+
+			existingSenders : map[string]bool{}
+			for _,sender := range p.Connections[i].PeerConnection.GetSenders(){
+				if sender.Track() == nil {
+					continue
+				}
+				
+				existingSenders[senders.Track().ID()] = true
+
+				if _, ok := p.TrackLocals[sender.Track().ID()]; !ok {
+					if err := p.Connections[i].PeerConnection.RemoveTrack(sender); err != nil{
+						return true
+					}
+				}
+			}
+
+			for _, receiver := range p.Connections[i].PeerConnection.GetReceivers(){
+				if receiver.Track() == nil{
+					continue
+				}
+				existingSenders[receiver.Track().ID()] = true
+			}
+
+			for trackID := range p.TrackLocals{
+				if _,ok := existingSenders[traclID]; !ok{
+					if _, err := p.Connetcions[i].PeerConnection.AddTrack(p.TrackLocals[trackID]; err != nil){
+						return true
+					}
+				}
+			}
+		}
+	}
+}
+
 func(p *Peers) dispatchKeyFrames(){
 
+}
+
+type WesocketMessage struct{
+	Event string `json:"Event`
+	Data string `json:"data"`
 }
